6665f65de1c1ec55ac2298d56975093d7011dde6
MatteoRossi01 <matteorossii6920@gmail.com>
2023-04-03 17:20:34 +0200

    Primo service implementato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { CardComponent } from './services/card/card.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/card/card.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { MyDataService } from '../my-data.service';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+
+  myData: any=[];
+  mySingleData: any=[];
+
+  constructor(private MyDataService: MyDataService) {};
+
+  ngOnInit(): void {
+
+    this.MyDataService.getData().subscribe((data:any) => {
+      this.myData = data.array;
+    });
+
+  };
+
+}
